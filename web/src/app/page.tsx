6c04d78bf4f88ac19234071125ddd8b76741a72b
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2023-05-19 13:01:49 -0300

    feat: criação do stripes linha de tempo

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,8 +2,12 @@ export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
       {/* Left */}
-      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden">
+      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10">
+        {/* Blur */}
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 -translate-y-1/2 translate-x-1/2 rounded-full blur-full" />
+
+        {/* Stripes */}
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes " />
       </div>
 
       {/* Right */}
