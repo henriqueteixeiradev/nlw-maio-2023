7eae7a667b2eedee119ce1128a2521639e74fe2c
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2023-05-19 18:09:51 -0300

    criação do crud e cors

--- SERVER/src/server.ts
+++ SERVER/src/server.ts
@@ -1,16 +1,13 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import cors from "@fastify/cors";
+import { memoriesRoutes } from "./routes/memories";
 
 const app = fastify();
 
-const prisma = new PrismaClient();
+app.register(memoriesRoutes);
 
-app.get("/users", async (request, reply) => {
-  const users = await prisma.user.findMany();
-
-  return {
-    users,
-  };
+app.register(cors, {
+  origin: true,
 });
 
 app
