c55cba56bf2cc21d55324348f227d0afdd6ef729
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2023-05-20 08:44:41 -0300

    fluxo de autenticação, config github Auth, Obtendo code do github Auth

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -15,7 +15,10 @@ export default function Home() {
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes " />
 
         {/* Sigin */}
-        <a href="" className="flex items-center gap-3 text-left">
+        <a
+          href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
+          className="flex items-center gap-3 text-left"
+        >
           <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
             <User className="h-5 w-5 text-gray-500" />
           </div>
