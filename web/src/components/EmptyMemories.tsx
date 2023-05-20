e5cdcbdee877a36bb66eb4d57fbb8082a2158345
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2023-05-20 08:52:32 -0300

    componentizei as partes da home page

---
+++ web/src/components/EmptyMemories.tsx
@@ -0,0 +1,13 @@
+export function EmptyMemories() {
+  return (
+    <div className="flex flex-1 items-center justify-center ">
+      <p className="text-center leading-relaxed w-[360px]">
+        Você ainda não registrou nenhuma lembraça.{" "}
+        <a href="/new" className="underline hover:text-gray-50">
+          criar agora
+        </a>
+        .
+      </p>
+    </div>
+  );
+}
