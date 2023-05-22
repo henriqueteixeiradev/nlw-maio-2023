c66a7d561f0c7a6d588d2f5eb678c03adc3b5fb5
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2023-05-22 08:45:51 -0300

    tela de new

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { View, Text, ImageBackground } from "react-native";
 import { SplashScreen, Stack } from "expo-router";
 import { styled } from "nativewind";
+import * as SecureStore from "expo-secure-store";
 
 import blurBg from "../src/assets/bg-blur.png";
 import Stripes from "../src/assets/stripes.svg";
@@ -13,15 +14,24 @@ import {
 
 import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 
 const StyledStripes = styled(Stripes);
 
 export default function Layout() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState(false);
+
   const [hasLoaderFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   });
+
+  useEffect(() => {
+    SecureStore.getItemAsync("token").then((token) => {
+      setIsUserAuthenticated(!!token);
+    });
+  }, []);
 
   if (!hasLoaderFonts) {
     return <SplashScreen />;
@@ -44,7 +54,11 @@ export default function Layout() {
             backgroundColor: "transparent",
           },
         }}
-      />
+      >
+        <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        <Stack.Screen name="new" />
+        <Stack.Screen name="memories" />
+      </Stack>
     </ImageBackground>
   );
 }
